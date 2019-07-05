11c6ebf11a802a99ab4e5ab5533cea914b9bacdc
Mike M <mikey.x88@gmail.com>
2019-07-04 22:13:55 -0400

    Fix Log() gRPC call

--- logging-grpc-client/client.py
+++ logging-grpc-client/client.py
@@ -16,7 +16,7 @@ stub = Log_pb2_grpc.LogServiceStub(channel)
 
 print("Connection up...")
 
-log1 = Log_pb2.Log()
+log1 = Log_pb2.LogRequest()
 log1.message = 'This is a sample log message.'
 log1.type = 'Info'
 
@@ -26,8 +26,8 @@ guid_1 = stub.Log(log1)
 print("Logged with id %s." % str(guid_1).strip())
 
 # Asynchronous
-# feature_future = stub.Log.future(log1)
-# print(feature_future)
-# feature = feature_future.result()
+guid_2_future = stub.Log.future(log1)
+guid_2 = guid_2_future.result()
+print("Logged with id %s." % str(guid_2).strip())
 
 channel.close()
